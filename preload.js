f42e003168b7a71a3e06b02837882e2133592d93
Martin Ludwig <ML.Commits@gmail.com>
2021-05-20 20:42:26 +0200

    added rich-text-editor (quilljs)

--- preload.js
+++ preload.js
@@ -1,2 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline'],
+        ['link']
+    ];
+    var options = {
+        modules: {
+            toolbar: toolbarOptions
+        },
+        theme: 'bubble'
+    };
+
+    var Quill = require('quill')
+
+
+    var editor = new Quill('#content', options);
 })
