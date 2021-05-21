a0d59fdee1c54c26ad817b2bb8d00541e422bf66
Martin Ludwig <ML.Commits@gmail.com>
2021-05-21 23:39:04 +0200

    added auto save and database (pouchdb)

--- preload.js
+++ preload.js
@@ -1,4 +1,34 @@
+const PouchDB = require('pouchdb');
+const db = new PouchDB('./data/local');
+
+const htmlContentDiv = '.ql-editor'
+
+let currentTab;
+let content;
+
+let keyupTimeout;
+let saveContentDelay = 700;
+
+// Get Content and put contents in editor
+async function SetContent(tabId, element) {
+    // Todo refactor GetTab and better error handling
+    currentTab = await GetTab(tabId);
+    console.log("init currentTab", currentTab)
+    console.log("init content", content)
+    if (currentTab && currentTab["contents"]) {
+        content = currentTab["contents"];
+    } else {
+        content = ''
+    }
+    document.querySelector(element).innerHTML = content;
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
+    SetContent('tab1', htmlContentDiv);
+
+    console.log("init currentTab", currentTab)
+    console.log("init content", content)
 
     var toolbarOptions = [
         ['bold', 'italic', 'underline'],
@@ -13,6 +43,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
     var Quill = require('quill')
 
-
     var editor = new Quill('#content', options);
+
+    // auto save
+    document.addEventListener('keyup', function (event) {
+        clearTimeout(keyupTimeout);
+        keyupTimeout = setTimeout(SaveContent, saveContentDelay);
+    })
 })
+
+function SaveContent() {
+    content = document.querySelector(htmlContentDiv).innerHTML;
+    console.log("SAVING NEW CONTENT= ", content)
+
+    UpsertTab('tab1', content)
+}
+
+function UpsertTab(tabId, content) {
+    return db.get(tabId).then(function (doc) {
+        return db.put({
+            _id: tabId,
+            _rev: doc._rev,
+            contents: content
+        });
+    }).then(function (response) {
+        // handle response
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+// TODO:
+// Put Initialize document in seperate function
+function GetTab(tabId) {
+    return db.get(tabId).then(function (doc) {
+        return doc;
+    }).catch(function (err) {
+        // Initialize document
+        return db.put({
+            _id: tabId,
+            contents: ""
+        }).then(function (response) {
+            // handle response
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+}
